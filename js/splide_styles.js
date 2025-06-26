08caf1c5ef465539dc2f1f806d9ca31a258c398a
Oleksandr Denkovych <84628629+booblyck@users.noreply.github.com>
2025-06-26 16:51:10 +0300

    Add files via upload

---
+++ js/splide_styles.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide(".splide", {
+        autoplay: true,
+        interval: 5000,
+        type: "loop",
+    }).mount();
+});
