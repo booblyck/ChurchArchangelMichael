631024a997a6a905c8f43a260f7471a56ac44061
Oleksandr Denkovych <84628629+booblyck@users.noreply.github.com>
2025-06-26 20:13:34 +0300

    Update splide_styles.js

--- js/splide_styles.js
+++ js/splide_styles.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     new Splide(".splide", {
         autoplay: true,
         interval: 5000,
         type: "loop",
+    }).mount();
+});*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide(".splide", {
+        type: "loop",
+        perPage: 1,
+        autoplay: true,
+        pauseOnHover: true,
+        arrows: true,
+        pagination: false,
+        breakpoints: {
+            768: {
+                perPage: 1,
+            },
+        },
     }).mount();
 });
